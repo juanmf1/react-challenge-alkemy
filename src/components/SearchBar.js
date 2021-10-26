e8558a9d79a1bcff4578cda703c65aef207bc8d4
Juan <juanmanuelfernandez2@outlook.com>
2021-10-26 19:48:43 -0300

    agregado formik a searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,28 +4,10 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
 import HeroCard from "./HeroCard";
-import {Formik} from "formik";
+import { Formik, Form, ErrorMessage, Field } from "formik";
 
-const SearchBar = ({showResults, setShowResults }) => {
-  const [query, setQuery] = useState("");
+const SearchBar = ({ showResults, setShowResults }) => {
   const [results, setResults] = useState([]);
-
-  const url = `https://superheroapi.com/api.php/10227809474243443/search/${query}`;
-
-  const handleChange = (event) => {
-    setQuery(event.target.value);
-  };
-
-  const searchHero = async () => {
-    axios.get(url).then((response) => {
-      setResults([]);
-      if (response.data.results) {
-        setResults(response.data.results);
-      }
-    });
-    setShowResults(true);
-  };
-
 
   return (
     <>
@@ -33,23 +15,64 @@ const SearchBar = ({showResults, setShowResults }) => {
         fluid
         className="search-container p-2 d-flex justify-content-center"
       >
-        <input type="text" value={query} onChange={handleChange} />
-        <Button className="ms-2" onClick={searchHero}>
-          {" "}
-          Search a superhero!
-        </Button>
+        <Formik
+          initialValues={{ query: "" }}
+          validate={(values) => {
+            let error = {};
+
+            if (values.query.length <= 1) {
+              error.query = "Enter two or more chars to search";
+            }
+            return error;
+          }}
+          onSubmit={(values, { resetForm }) => {
+            const url = `https://superheroapi.com/api.php/10227809474243443/search/${values.query}`;
+
+            if (values.query.length > 1) {
+              axios.get(url).then((response) => {
+                setResults([]);
+                if (response.data.results) {
+                  setResults(response.data.results);
+                }
+              });
+              setShowResults(true);
+              resetForm();
+            }
+            resetForm();
+          }}
+        >
+          {({ errors }) => (
+            <Form className="d-flex flex-column">
+              <div className="d-flex">
+                <Field type="text" name="query"></Field>
+                <Button type="submit" className="ms-2">
+                  Search a superhero!
+                </Button>
+              </div>
+              <ErrorMessage
+                name="query"
+                component={() => (
+                  <div className="error-login p-1 mt-3">{errors.query}</div>
+                )}
+              ></ErrorMessage>
+            </Form>
+          )}
+        </Formik>
       </Container>
       <Container fluid className="results-container">
-      <div className="d-flex flex-wrap justify-content-center align-content-center">
-        {showResults && (
-          results.length === 0 ? (
-          <p className="m-2" style={{ fontWeight: "bolder", fontSize: "1.5rem" }}>
-            No heroes found
-          </p>
-        ) : (
-          results.map((hero) => <HeroCard mode="search" id={hero.id} />)
-        ))}
-      </div>
+        <div className="d-flex flex-wrap justify-content-center align-content-center">
+          {showResults &&
+            (results.length === 0 ? (
+              <p
+                className="m-2"
+                style={{ fontWeight: "bolder", fontSize: "1.5rem" }}
+              >
+                No heroes found
+              </p>
+            ) : (
+              results.map((hero) => <HeroCard mode="search" id={hero.id} />)
+            ))}
+        </div>
       </Container>
     </>
   );
